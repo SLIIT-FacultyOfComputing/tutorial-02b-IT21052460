172a2575ab093c90156f1c25043a3640e50cc965
chandula Nanayakkara <87331884+IT21052460@users.noreply.github.com>
2021-07-26 03:45:12 +0000

    nothing

--- tute04.cpp
+++ tute04.cpp
@@ -11,6 +11,10 @@ long Factorial(int no);
 long nCr(int n, int r);
 Do not modify the main function.*/
 
+
+
+//Ex 4
+
 #include <iostream>
 
 long Factorial(int no);
@@ -27,3 +31,22 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no)
+{   
+   int i;
+   long ans=1;
+  for(i=1;i<=no;i++)
+   {
+     ans=i*ans;
+   }
+ return ans;
+	
+}
+ 
+long nCr(int n, int r)
+{
+  long q= Factorial(n)/(Factorial(r)*Factorial(n-r));
+  return q;
+  
+ }
